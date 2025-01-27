ff347716ff8807dc2fa40b5eb2cb3d91895d3ac1
Shubham Sharma <shubhamsharma.emails@gmail.com>
2025-01-27 03:29:17 +0000

    chore: improve package structure

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,25 @@
+"""
+passgen - A secure password generation and management tool.
+Created by Shubham Sharma (https://github.com/Sharma-IT)
+"""
 
+from .password_generator import (
+    VERSION,
+    Config,
+    Console,
+    PasswordGenerator,
+    FileHelper,
+)
+
+__version__ = VERSION
+__author__ = "Shubham Sharma"
+__email__ = "shubhamsharma.emails@gmail.com"
+__url__ = "https://github.com/Sharma-IT/passgen"
+
+__all__ = [
+    "VERSION",
+    "Config",
+    "Console",
+    "PasswordGenerator",
+    "FileHelper",
+]
